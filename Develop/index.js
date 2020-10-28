b154994e8dbfe6bdcc458825f8f80add4b68c6b4
Justin Gabriele <gabrielejustin@yahoo.com>
2020-10-28 19:22:08 -0400

    starting variables(requires)

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,16 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
-];
+// array of questions for user
+const questions = [];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
